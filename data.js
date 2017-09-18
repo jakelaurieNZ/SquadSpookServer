694fda764cde39c9b7309acaa3bc1f2bbb616e93
Jake Laurie <jake.gordon.laurie@gmail.com>
2017-09-18 20:01:10 +1200

    Increase cache redis cache time to wait for new data from socket

--- data.js
+++ data.js
@@ -25,7 +25,8 @@ function init(socketAddress) {
         var server = JSON.parse(data)['server']
 
         if(server != null) {
-            client.set(server['id'], data, 'EX', 120); //Expires in two minutes
+            console.log(server['id'], new Date())
+            client.set(server['id'], data, 'EX', 240); //Expires in four minutes
         } else {
             console.log('fail')
         }
